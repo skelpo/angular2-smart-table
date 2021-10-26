71cb428c2701dda4a013929da119a5a5496fdae0
Carmelo <carmelofiorello@gmail.com>
2021-10-26 21:29:49 +0200

    Fixed some style in demo

--- projects/angular2-smart-table/src/lib/angular2-smart-table.module.ts
+++ projects/angular2-smart-table/src/lib/angular2-smart-table.module.ts
@@ -5,6 +5,7 @@ import { Angular2SmartTableComponent } from './angular2-smart-table.component';
 import { CellModule } from './components/cell/cell.module';
 import { FilterModule } from './components/filter/filter.module';
 import { PagerModule } from './components/pager/pager.module';
+import { TabsModule } from './components/tags/tags.module';
 import { TBodyModule } from './components/tbody/tbody.module';
 import { THeadModule } from './components/thead/thead.module';
 
@@ -23,6 +24,7 @@ import { THeadModule } from './components/thead/thead.module';
     PagerModule,
     TBodyModule,
     THeadModule,
+    TabsModule
   ],
   exports: [
     Angular2SmartTableComponent
--- projects/demo/src/app/shared/components/header/header.component.ts
+++ projects/demo/src/app/shared/components/header/header.component.ts
@@ -2,6 +2,14 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'header-component',
+  styles: [`
+    li::marker {
+      content: '';
+    }
+    ul {
+      padding: 0;
+    }
+  `],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
